f26c63559050b9e32a0680ee316c1103815b051a
cmmerritt <christiane.merritt@gmail.com>
2021-08-16 16:35:52 -0700

    color input test passes

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { screen, render, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
   afterEach(() => cleanup());
+
   it('renders App', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  // get color input
+  // fire change event on color input with newColor
+  // expect display to have background color === newColor
+  it('gets color input', () => {
+    render(<App />);
+    const colorInput = screen.getByTestId('input');
+    console.log(colorInput);
+    fireEvent.change(colorInput, { target: { value: '#0000FF' } });
+    expect(colorInput.value).toBe('#0000ff');
   });
 });
